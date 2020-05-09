01ac843ea4be581f1bb9894dbc6df5f46ba0b93a
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2020-05-09 10:21:15 +0200

    Akka timing added.

---
+++ src/main/java/com/racing/akked/App.java
@@ -0,0 +1,14 @@
+package com.racing.akked;
+
+import akka.actor.typed.ActorSystem;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        final ActorSystem<RaceController.Command> bigPrimesSystem =
+                ActorSystem.create(RaceController.create(), "RaceSimulation");
+
+        bigPrimesSystem.tell(new RaceController.StartCommand());
+    }
+}
--- src/main/java/com/racing/akked/RaceController.java
+++ src/main/java/com/racing/akked/RaceController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +21,6 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
 
     public interface Command extends Serializable {};
 
-    @Value
     public static class StartCommand implements Command {
         public static final long serialVersionUID = 1L;
     }
@@ -30,6 +30,10 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
         public static final long serialVersionUID = 1L;
         ActorRef<Racer.Command> racer;
         int position;
+    }
+
+    private class GetPositionsCommand implements Command {
+        public static final long serialVersionUID = 1L;
     }
 
     private RaceController(ActorContext<RaceController.Command> context) {
@@ -43,6 +47,7 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
     private Map<ActorRef<Racer.Command>, Integer> currentPositions;
     private long start;
     private int raceLength = 100;
+    private Object TIMER_KEY;
 
     @Override
     public Receive<Command> createReceive() {
@@ -56,6 +61,18 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
                         currentPositions.put(racer, 0);
                         racer.tell(new Racer.StartCommand(raceLength));
                     }
+
+                    return Behaviors.withTimers(timer -> {
+                        timer.startTimerAtFixedRate(TIMER_KEY, new GetPositionsCommand(), Duration.ofSeconds(1));
+                        return this;
+                    });
+                })
+
+                .onMessage(GetPositionsCommand.class, command -> {
+                    currentPositions.keySet().forEach(
+                        racer -> racer.tell(new Racer.PositionCommand(getContext().getSelf()))
+                    );
+                    displayRace();
                     return this;
                 })
 
@@ -65,5 +82,17 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
                 })
 
                 .build();
+    }
+
+    private void displayRace() {
+        int displayLength = 160;
+        for (int i = 0; i < 50; ++i) System.out.println();
+        System.out.println("Race has been running for " + ((System.currentTimeMillis() - start) / 1000) + " seconds.");
+        System.out.println("    " + new String (new char[displayLength]).replace('\0', '='));
+        int i = 0;
+        for (ActorRef<Racer.Command> racer : currentPositions.keySet()) {
+            System.out.println(i + " : "  + new String (new char[currentPositions.get(racer) * displayLength / 100]).replace('\0', '*'));
+            i++;
+        }
     }
 }
